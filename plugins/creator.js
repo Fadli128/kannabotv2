58b68d0aca9f0d999fac6789c9e1f89b917e805d
Fadli128 <108339471+Fadli128@users.noreply.github.com>
2022-07-06 07:31:26 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,11 +20,11 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* Dani Offcial
-*ᴀɢᴇ:* 15ᵗʰ
+*ɴᴀᴍᴇ:* Ary Offcial
+*ᴀɢᴇ:* 16ᵗʰ
 *sᴛᴀᴛᴜs:* Hanya Butuh Pasangan:v 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 16 Desember 2007
+*ʙɪʀᴛʜᴅᴀʏ:* 5 Mei 2006
 *ᴀᴅᴅʀᴇss:* Indonesia, Kalsel, Kandangan
 
 *- - sᴋɪʟʟs: - -* 
